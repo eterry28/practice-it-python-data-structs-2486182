0f700d2ae5861116dc493fada55c72889b37490f
Eric Terry <eric.terry.kc@gmail.com>
2023-08-14 20:56:34 +0000

    completed section 3

--- 03_01b.py
+++ 03_01b.py
@@ -1,9 +1,20 @@
+from collections import namedtuple
+
+def can_take_order(driver, num_packages):
+    return driver.car_capacity >= num_packages
 
 def main():
     #add code here
     #create a driver with a name, car type, and car capacity
+    Driver = namedtuple("driver", ["name", "car_type", "car_capacity"])
     #an example you can use to practice: "Iris", "Toyota Prius", 7
+    iris = Driver("Iris", "Prius", 7)
+    mickey = Driver("Micky", "Tucson", 15)
+    woody = Driver("Woody", "Hummer", 26)
     #check if they can take a certain order, given their car's capacity.
+    print(can_take_order(woody, 3))
+    print(can_take_order(mickey, 20))
+    print(can_take_order(iris, 10))
     return
 
 if __name__ == "__main__":
--- 03_02b.py
+++ 03_02b.py
@@ -1,8 +1,21 @@
+import csv
+from collections import namedtuple
 
 def main():
     #add code here
     #read data/Customer.csv
+    with open("data/Customer.csv", "r") as customers:
+        csv_reader = csv.reader(customers, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                Customer = namedtuple("Customer", ",".join(row))
+            else:
+                customer = Customer(*row)
+                print(customer)
+            line_count += 1
     #Create workable objects with each line
+    
     return
 
 if __name__ == "__main__":
